f7d1121d85a6df1020c590f99532b8a9ed0b8593
Y.Naveen Lakshan De Silva <139128804+lakshannaveen@users.noreply.github.com>
2025-03-02 16:47:20 +0530

    Add files via upload

---
+++ SDT frontend/assets/js/login.js
@@ -0,0 +1,11 @@
+document.getElementById('loginForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+
+    if (username === "admin" && password === "password") {
+        window.location.href = "dashboard.html";
+    } else {
+        alert("Invalid login credentials!");
+    }
+});
