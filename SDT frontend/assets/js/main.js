f810cbd554733a3f2f4e9cbd220f5d90b1bc363c
lakshannaveen <139128804+lakshannaveen@users.noreply.github.com>
2025-03-13 18:19:30 +0530

    add styles to  main page

--- SDT frontend/assets/js/main.js
+++ SDT frontend/assets/js/main.js
@@ -2,8 +2,8 @@ console.log("Main JS file loaded!");
 
 document.addEventListener("DOMContentLoaded", () => {
     const welcomeText = document.getElementById("welcome-text");
-    
-    // Add typing animation effect
+
+    // Typing Animation Effect
     let text = "Welcome to the Air Quality Monitoring Dashboard";
     let index = 0;
 
@@ -17,16 +17,12 @@ document.addEventListener("DOMContentLoaded", () => {
 
     typeText();
 
-    // Click event logic
+    // Click Event Logic for Secret Admin Page
     let clickCount = 0;
 
-    // Add click event listener
     welcomeText.addEventListener("click", function() {
         clickCount++;
-
-        // Check if the heading has been clicked 5 times
         if (clickCount >= 5) {
-            // Navigate to adminlogin.html
             window.location.href = "/SDT frontend/admin pages/adminlogin.html";
         }
     });
